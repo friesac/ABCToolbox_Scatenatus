c8e960da2d765d990d85bbbff0f9d9ab9626112d
Anthony Fries <friesac@gmail.com>
2016-03-11 15:06:56 -0500

    Bug fixes for MCMC shell

--- ABCToolbox_mcmc.sh
+++ ABCToolbox_mcmc.sh
@@ -5,37 +5,37 @@
 # Call it this way: ./ABCToolbox_mcmc.sh [path to results directory] [population] [model] [sample size] [# of simulations to run] [tolerance] [walltime - format HH:MM:SS]
 
 #------ Specify the parameters of the run in the command line submission -----
-pathToResults=$1
-population=$2
-model=$3
-sampleSize=$4
-simulations=$5
-tolerance=$6
-walltime=$7
+pathToResults=$1	# Path to results files from Calibration run.
+population=$2		# The population that you're working on.
+model=$3		# The demographic model being used.
+sampleSize=$4		# The haploid size of the dataset.
+simulations=$5		# The number of simulations that you'd like to try.
+tolerance=$6		# The tolerance value you'd like to use for MCMC searching.
+walltime=$7		# The walltime for a qsub submission.
 
 #------ Specify the parameters for the OSC -----
-nodes=1
-ppn=1
-memory=4gb # For ABCToolbox you probably don't need more than 4mb
-approx_run_date=$(date +%F-%k-%M) # Capture the date and time variables for naming directories.
-typeOfArlequinFileToUse="single_pop" # [single_pop/pairwise_pop] to specify how the calculations are processed.
-messageCode="a" #"b"egin,"e"nd,"a"borted,"s"uspended,"n"one - example ="bae"
-jobarrays=1 # How many iterations of that exact run do you want... i.e. this is relevant to the mcmc search/indicates multiple starting chains.
+nodes=1					# Total nodes that you'd use --- should not need more than 1 for simcoal.
+ppn=1					# Total processors to use on that node.
+memory=4gb 				# For ABCToolbox you probably don't need more than 4mb
+approx_run_date=$(date +%F-%k-%M) 	# Capture the date and time variables for naming directories.
+typeOfArlequinFileToUse="single_pop" 	# [single_pop/pairwise_pop] to specify how the calculations are processed.
+messageCode="a" 			#"b"egin,"e"nd,"a"borted,"s"uspended,"n"one - example ="bae"
+jobarrays=2 				# How many iterations of that exact run do you want... i.e. this is relevant to the mcmc search/indicates multiple starting chains.
 
-#------ Just some formatting for the script -----
+#------ Just some formatting for the script output -----
 bold=$(tput bold)
 normal=$(tput sgr0)
 
 # Go to the main results directory
-cd $pathToResults
+cd $HOME/Scate_msat/$pathToResults	# This is the main calibration outout directory.
 
+# Start printing to the screen - Youre checking some of the details.
 echo ""
 echo "##################################################################################################################"
 echo "##################################################################################################################"
 echo "##################################################################################################################"
 echo ""
 echo "Current Directory: ${bold}$PWD${normal}"
-currDir=$PWD
 echo ""
 echo "Details of Run"
 echo "-----------------"
@@ -44,36 +44,41 @@ echo "Population = ${population}"
 echo "Independent Chains = ${jobarrays}"
 echo "Simulations = ${simulations}"
 echo "2N individuals = ${sampleSize}"
+echo "Calibration directory used = ${pathToResults}"
+echo "-----------------"
 echo ""
-mkdir $currDir/${population}_${model}_MCMC 2>/dev/null
-cd $currDir/${population}_${model}_MCMC
-echo ""
-echo "Results will be sent to ${bold}${PWD}${normal}"
-echo ""
-currDir=$PWD
 
-# Edit the mcmc input file to contain information from your command line inputs
+# Make a directory to hold all of the MCMC results.
+mkdir $HOME/Scate_msat/${pathToResults}${population}_${model}_MCMC 2>/dev/null	# Youre creating a directory within the calibration directory to start placing output.
+cd $HOME/Scate_msat/${pathToResults}${population}_${model}_MCMC			# change into that new directory.
+echo ""
+echo "Results will be sent to ${bold}${PWD}${normal}"	# This better be the file you just created.
+echo ""
+
+# Edit the ABCsampler mcmc input file to contain information from your command line inputs
 echo "... updating ABC mcmc_input file with run parameters"
 echo ""
-cp $HOME/Scate_msat/template_mcmc.input ${model}_mcmc.input # Copy and rename the template mcmc input file.
-sed "s/dummy_model/$model/g" -i ${model}_mcmc.input
-sed "s/dummy_pop/$population/g" -i ${model}_mcmc.input
-sed "s/dummy_sims/$simulations/g" -i ${model}_mcmc.input
-sed "s/dummy_tolerance/$tolerance/g" -i ${model}_mcmc.input
+cp $HOME/Scate_msat/template_mcmc.input ${model}_mcmc.input 	# Copy and rename the template mcmc input file into that just made directory
+sed "s/dummy_model/$model/g" -i ${model}_mcmc.input		# Replace the model name in the input file.
+sed "s/dummy_pop/$population/g" -i ${model}_mcmc.input		# Replace the population name in the input file.
+sed "s/dummy_sims/$simulations/g" -i ${model}_mcmc.input	# Replace the # of simulations in the input file.
+sed "s/dummy_tolerance/$tolerance/g" -i ${model}_mcmc.input	# Replace the tolerance limit in the input file.
 
+# Edit the par file with the sample size.
 echo "... updating the par file with correct sample size"
 echo ""
-cp $HOME/Scate_msat/models/est/${model}.est .
-cp $HOME/Scate_msat/models/par/${model}.par .
-sed "s/dummy_pop_size/$sampleSize/g" -i ${model}.par
+cp $HOME/Scate_msat/models/est/${model}.est .			# Copy the .est file with the priors to the created directory.
+cp $HOME/Scate_msat/models/par/${model}.par .			# Copy the .par file to the created directory.
+sed "s/dummy_pop_size/$sampleSize/g" -i ${model}.par		# Replace the sample size in the .par file.
 
-jobName=${population}_${model}_mcmc.sh
+jobName=${population}_${model}_mcmc.sh		# This is going to hold the name of the shell script that is going to be created.
 
 echo "... making the bash file on the fly"
 echo ""
 (
-echo "#!/bin/bash" 
+echo "#!/bin/bash"
 echo ""
+# Resources needed for the HPC
 echo "# Specify the resources needed"
 echo "#PBS -A PAS0656"
 echo "#PBS -l walltime=${walltime}"
@@ -83,32 +88,27 @@ echo "#PBS -l mem=${memory}"
 echo "#PBS -m ${messageCode}"
 echo ""
 echo "set -x"
-
-echo "mkdir ${model}_mcmc_${PBS_ARRAYID} 2>/dev/null"
-
+# Make sure the right files are in the newly created directory.
+echo "mkdir $HOME/Scate_msat/${pathToResults}${population}_${model}_MCMC/${model}_mcmc_PBS_ARRAYID 2>/dev/null"
 echo "cp $HOME/Scate_msat/obs/Scate_17micro_${population}.obs \$TMPDIR"
-echo "cp $HOME/Scate_msat/arl/${typeOfArlequinFileToUse}/arl_run.ars \$TMPDIR" 
-echo "cp $HOME/Scate_msat/arl/${typeOfArlequinFileToUse}/arl_run.txt \$TMPDIR" 
-echo "cp $HOME/Scate_msat/arl/${typeOfArlequinFileToUse}/ssdefs.txt \$TMPDIR" 
+echo "cp $HOME/Scate_msat/arl/${typeOfArlequinFileToUse}/arl_run.ars \$TMPDIR"
+echo "cp $HOME/Scate_msat/arl/${typeOfArlequinFileToUse}/arl_run.txt \$TMPDIR"
+echo "cp $HOME/Scate_msat/arl/${typeOfArlequinFileToUse}/ssdefs.txt \$TMPDIR"
 echo "cp $HOME/Scate_msat/arl/arlsumstat \$TMPDIR"
 echo "cp $HOME/Scate_msat/binaries/simcoal2 \$TMPDIR"
 echo "cp $HOME/Scate_msat/binaries/ABCsampler \$TMPDIR"
-echo "cp ${model}_mcmc.input \$TMPDIR"
-echo "cp ${model}.par \$TMPDIR"
-echo "cp ${model}.est \$TMPDIR"
-echo "cp ../${population}_output_sampling1.txt \$TMPDIR"
-echo "cp ../Routput_${model}_output_sampling1.txt \$TMPDIR"
-echo "cp ${model}.par \$TMPDIR"
-
-echo "cd $TMPDIR"
+echo "cp $HOME/Scate_msat/${pathToResults}${population}_${model}_MCMC/${model}_mcmc.input \$TMPDIR"
+echo "cp $HOME/Scate_msat/${pathToResults}${population}_${model}_MCMC/${model}.par \$TMPDIR"
+echo "cp $HOME/Scate_msat/${pathToResults}${population}_${model}_MCMC/${model}.est \$TMPDIR"
+echo "cp $HOME/Scate_msat/${pathToResults}${population}_output_sampling1.txt \$TMPDIR"
+#echo "cp $HOME/Scate_msat/${pathToResults}Routput_${model}_output_sampling1.txt \$TMPDIR"
+echo "cd \$TMPDIR"
 
 echo "chmod +x ABCsampler simcoal2 arlsumstat"
-echo "echo Running: ./ABCsampler ${model}_mcmc.input addToSeed=$PBS_ARRAYID"
-echo "./ABCsampler ${model}_mcmc.input addToSeed=${PBS_ARRAYID}"
+echo "./ABCsampler ${model}_mcmc.input addToSeed=PBS_ARRAYID"	# You're adding to the seed so that the random start point is different between the jobs on the array.
 echo ""
-echo "cp -R * $currDir/${model}_mcmc_${PBS_ARRAYID}"
-
-echo "cd $HOME/Scate_msat/${pathToResults/${model}_mcmc_${PBS_ARRAYID}"
+echo "cp -R * $HOME/Scate_msat/${pathToResults}${population}_${model}_MCMC/${model}_mcmc_PBS_ARRAYID"
+echo "cd $HOME/Scate_msat/${pathToResults}${population}_${model}_MCMC/${model}_mcmc_PBS_ARRAYID"
 echo "rm simcoal2"
 echo "rm ABCsampler"
 echo "rm arlsumstat"
@@ -116,9 +116,13 @@ echo "rm arl_run.ars"
 echo "rm arl_run.txt"
 echo "rm ssdefs.txt"
 )> ${jobName}
+
+sed "s/PBS_ARRAYID/\$PBS_ARRAYID/g" -i ${jobName}
 chmod +x ${jobName}
 echo ""
 echo "Bash file ${bold}${jobName}${normal} created"
 echo ""
 echo ""
-# qsub ./$jobName
+
+qsub -t 1-${jobarrays} ${jobName}
+
---
+++ population_results/SSSP_Model1_2016-03-09-13-04/SSSP_Model1_MCMC/SSSP_Model1_mcmc.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Specify the resources needed
+#PBS -A PAS0656
+#PBS -l walltime=00:01:00
+#PBS -l nodes=1:ppn=1
+#PBS -N ABC_SSSP_Model1_2016-03-11-15-03
+#PBS -l mem=4gb
+#PBS -m a
+
+set -x
+mkdir /nfs/12/osu0378/Scate_msat/population_results/SSSP_Model1_2016-03-09-13-04/SSSP_Model1_MCMC/Model1_mcmc_$PBS_ARRAYID 2>/dev/null
+cp /nfs/12/osu0378/Scate_msat/obs/Scate_17micro_SSSP.obs $TMPDIR
+cp /nfs/12/osu0378/Scate_msat/arl/single_pop/arl_run.ars $TMPDIR
+cp /nfs/12/osu0378/Scate_msat/arl/single_pop/arl_run.txt $TMPDIR
+cp /nfs/12/osu0378/Scate_msat/arl/single_pop/ssdefs.txt $TMPDIR
+cp /nfs/12/osu0378/Scate_msat/arl/arlsumstat $TMPDIR
+cp /nfs/12/osu0378/Scate_msat/binaries/simcoal2 $TMPDIR
+cp /nfs/12/osu0378/Scate_msat/binaries/ABCsampler $TMPDIR
+cp /nfs/12/osu0378/Scate_msat/population_results/SSSP_Model1_2016-03-09-13-04/SSSP_Model1_MCMC/Model1_mcmc.input $TMPDIR
+cp /nfs/12/osu0378/Scate_msat/population_results/SSSP_Model1_2016-03-09-13-04/SSSP_Model1_MCMC/Model1.par $TMPDIR
+cp /nfs/12/osu0378/Scate_msat/population_results/SSSP_Model1_2016-03-09-13-04/SSSP_Model1_MCMC/Model1.est $TMPDIR
+cp /nfs/12/osu0378/Scate_msat/population_results/SSSP_Model1_2016-03-09-13-04/SSSP_output_sampling1.txt $TMPDIR
+cd $TMPDIR
+chmod +x ABCsampler simcoal2 arlsumstat
+./ABCsampler Model1_mcmc.input addToSeed=$PBS_ARRAYID
+
+cp -R * /nfs/12/osu0378/Scate_msat/population_results/SSSP_Model1_2016-03-09-13-04/SSSP_Model1_MCMC/Model1_mcmc_$PBS_ARRAYID
+cd /nfs/12/osu0378/Scate_msat/population_results/SSSP_Model1_2016-03-09-13-04/SSSP_Model1_MCMC/Model1_mcmc_$PBS_ARRAYID
+rm simcoal2
+rm ABCsampler
+rm arlsumstat
+rm arl_run.ars
+rm arl_run.txt
+rm ssdefs.txt
